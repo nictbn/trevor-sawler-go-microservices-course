7e91a580ed0348d5d04bb85000c153c86b532c1d
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-26 16:48:41 +0200

    Broker code

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8080"
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s", webPort)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
---
+++ broker-service/go.mod
@@ -0,0 +1,8 @@
+module example.com/broker
+
+go 1.25.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+)
