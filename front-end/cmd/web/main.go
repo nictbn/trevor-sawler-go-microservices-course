2ec297d448949a93306cb93ca98a4be61c0ecc51
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-26 17:37:12 +0200

    Adding a docker file for the broker

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -7,13 +7,15 @@ import (
 	"net/http"
 )
 
+const webPort = "8081"
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
 
-	fmt.Println("Starting front end service on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	fmt.Printf("Starting front end service on port %s\n", webPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", webPort), nil)
 	if err != nil {
 		log.Panic(err)
 	}
