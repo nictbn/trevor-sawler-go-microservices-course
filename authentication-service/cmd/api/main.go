fff06c6f258c2df79203e07581718ec27a521cef
Nicu Taban <nictbndeveloper@gmail.com>
2026-01-27 15:14:09 +0200

    Authentication service stub

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"example.com/authentication/data"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("Starting authentication service")
+
+	app := Config{}
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
---
+++ authentication-service/go.mod
@@ -0,0 +1,9 @@
+module example.com/authentication
+
+go 1.25.5
+
+require (
+	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/go-chi/cors v1.2.2 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+)
